1e49dfb5eaee08d0dd57065b2fc1adf6b9a19a11
Darián López Utra <daroxs95@gmail.com>
2024-03-19 15:38:43 +0100

    Improve performance
    - Add lazy loading to out of initial viewport images
    - Add alt attribute to images
    - Add minification to custom scripts
    - Add image compression

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 - Refactor folder structure
 - Add vite to the project
 - Use public folder to store some static files
+- Add `type="module"` to some non minified scripts for vite to be able ton bundle and optimize them
+- Other scripts like jquery and owl carousel are left as they are, because they are already minified, optimized and
+  placed at the end of body tag. Also they depend on jquery so they can't be async
+- Add `vite-plugin-image-optimizer` for image and vector graphics optimization/compression
+- Any script tag with `type="module"` is treated as a JavaScript module and is deferred by default, so usage
+  of `main.js` and `contact.js` is already deferred by being builded and optimized by vite
+- Add `loading="lazy"` to all images not intended to be on page landing view (for example the first carousel image and
+  the two special offer ones in home page are set to `eager`)
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,24 @@
 import {defineConfig} from 'vite'
+import {resolve} from 'path';
+import {ViteImageOptimizer} from "vite-plugin-image-optimizer";
 
 export default defineConfig({
+    base: './',
+    plugins: [ViteImageOptimizer({
+        jpg: {
+            quality: 80,
+        }
+    })],
     build: {
+        minify: 'esbuild',
         rollupOptions: {
             input: {
-                cart: 'cart.html',
-                checkout: 'checkout.html',
-                contact: 'contact.html',
-                detail: 'detail.html',
-                index: 'index.html',
-                shop: 'shop.html',
+                cart: resolve(__dirname, 'cart.html'),
+                checkout: resolve(__dirname, 'checkout.html'),
+                contact: resolve(__dirname, 'contact.html'),
+                detail: resolve(__dirname, 'detail.html'),
+                index: resolve(__dirname, 'index.html'),
+                shop: resolve(__dirname, 'shop.html'),
             }
         }
     }
